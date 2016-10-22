2658a8135ee2305d5ba363a48379938671f10483
Joe Lanman <joelanman@gmail.com>
2016-10-22 11:58:28 +0100

    get files from repo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var GitHubApi = require('github')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
 
-module.exports = router;
+  var github = new GitHubApi({
+    debug: true,
+    headers: {
+      'user-agent': 'github-gallery' // GitHub is happy with a unique user agent
+    }
+  })
+
+  github.repos.getContent({
+    'owner': 'UKHomeOffice',
+    'repo': 'posters',
+    'path': ''
+  }, function (err, files){
+      console.log(JSON.stringify(files, null, '  '))
+      res.render('index', { files: files })
+  })
+
+})
+
+module.exports = router
