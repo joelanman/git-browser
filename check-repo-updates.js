f2508a303acda343d53e3e2cfc3ddbd4d0bc4172
Joe Lanman <joelanman@gmail.com>
2017-11-19 12:19:03 +0000

    update db after images processed

--- check-repo-updates.js
+++ check-repo-updates.js
@@ -26,34 +26,30 @@ log('check-repo-updates')
 const throttledGetLatestCommitDate = pThrottle(getLatestCommitDate, 1, 2000)
 const throttledGetRepoMap = pThrottle(getRepoMap, 1, 2000)
 
-processRepoMap = function(row, data){
+processRepoMap = function(row, data, lastCommitDate){
 
   log(`processRow`)
 
   return new Promise((resolve, reject) => {
 
-    // put map in db
-    var now = new Date()
+    // process images
+    githubToS3(row.owner, row.name, data.filesToConvert)
+    .then(function(){
 
-    db('repos')
-      .where('id', row.id)
-      .update({
-        'last_updated': now.toISOString(),
-        'map': data.map
-        // ERROR map is updated before the actual images generated
-      }).then(function(){
-        log('done db update')
-        // process images
-        githubToS3(row.owner, row.name, data.filesToConvert)
-        .then(function(){
+      // update db
+      db('repos')
+        .where('id', row.id)
+        .update({
+          'last_updated': lastCommitDate.toISOString(),
+          'map': data.map
+        }).then(function(){
           resolve()
         })
-        .catch(function(error) {
-          // to do should we roll back the updated date?
-          console.error(error)
-          reject()
-        })
-      })
+    })
+    .catch(function(error) {
+      console.error(error)
+      reject()
+    })
   })
 }
 
@@ -66,39 +62,26 @@ const processRow = function(row){
   email.text += `${row.owner} - ${row.name} \n`
 
   return new Promise((resolve, reject) => {
-
-    if (row.last_updated === null){
-      log(`No last_updated, processing repo`)
-      email.text += `No last_updated, processing repo \n`
-      throttledGetRepoMap(row.owner, row.name).then(
-        function(data){
-          processRepoMap(row, data).then(resolve)
-          // done
-        }
-      )
-    } else {
-      throttledGetLatestCommitDate(row.owner, row.name).then(
-        function(date){
-          log(`comparing dates`)
-          date = dateFns.parse(date)
-          last_updated = dateFns.parse(row.last_updated)
-          if (date > row.last_updated){
-            log('updated since last_updated')
-            email.text += `updated since last_updated \n`
-            throttledGetRepoMap(row.owner, row.name).then(
-              function(data){
-                processRepoMap(row, data).then(resolve)
-                // done
-              }
-            )
-          } else {
-            log('no changes - skipping')
-            email.text += `no changes - skipping \n`
-            resolve()
+    throttledGetLatestCommitDate(row.owner, row.name).then(function(lastCommitDate){
+      log(`comparing dates`)
+      lastCommitDate  = dateFns.parse(lastCommitDate)
+      let lastUpdated = dateFns.parse(row.last_updated)
+      if (lastCommitDate > lastUpdated){
+        log('new commit since last updated')
+        email.text += `new commit since last updated \n`
+        throttledGetRepoMap(row.owner, row.name).then(
+          function(data){
+            processRepoMap(row, data, lastCommitDate)
+            .then(resolve)
+            // done
           }
-        }
-      )
-    }
+        )
+      } else {
+        log('no changes - skipping')
+        email.text += `no changes - skipping \n`
+        resolve()
+      }
+    })
   })
 }
 
